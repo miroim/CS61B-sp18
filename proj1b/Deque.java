96313ebf449e7477e6f7c5eda58660ccb9afaaad
Miro <hi@miro.im>
2024-04-13 23:58:08 +0800

    add interface Deque

---
+++ proj1b/Deque.java
@@ -0,0 +1,14 @@
+public interface Deque<T> {
+    void addFirst(T item);
+    void addLast(T item);
+    T removeFirst();
+    T removeLast();
+    T get(int index);
+    int size();
+    default boolean isEmpty() {
+        return size() == 0;
+    };
+    void printDeque();
+}
+
+
