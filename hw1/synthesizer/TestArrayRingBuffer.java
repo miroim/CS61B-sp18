f2bea7a74c4f81a0dcee5cfb9c71b8896c048ca7
Miro <hi@miro.im>
2024-04-18 17:16:00 +0800

    hw1: finished to Task 4

--- hw1/synthesizer/TestArrayRingBuffer.java
+++ hw1/synthesizer/TestArrayRingBuffer.java
@@ -9,7 +9,39 @@ import static org.junit.Assert.*;
 public class TestArrayRingBuffer {
     @Test
     public void someTest() {
-        //ArrayRingBuffer arb = new ArrayRingBuffer(10);
+        ArrayRingBuffer arb = new ArrayRingBuffer(10);
+        assertTrue(arb.isEmpty());
+        assertFalse(arb.isFull());
+        for (int i = 0; i < 10; i += 1) {
+            arb.enqueue(i);
+        }
+        assertEquals(0, arb.peek());
+        assertFalse(arb.isEmpty());
+        assertTrue(arb.isFull());
+
+        for (int i = 0; i < 10; i += 1) {
+            assertEquals(i, arb.dequeue());
+        }
+        assertTrue(arb.isEmpty());
+        assertFalse(arb.isFull());
+
+    }
+
+    @Test
+    public void testIterator() {
+        ArrayRingBuffer<Integer> arb = new ArrayRingBuffer(10);
+        for (int i = 0; i < 10; i += 1) {
+            arb.enqueue(i);
+        }
+        arb.dequeue();
+        arb.dequeue();
+        arb.enqueue(10);
+        arb.enqueue(11);
+        int n = 2;
+        for (int num : arb) {
+            assertEquals(n, num);
+            n += 1;
+        }
     }
 
     /** Calls tests for ArrayRingBuffer. */
