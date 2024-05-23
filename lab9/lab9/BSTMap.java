6f4e0edac62d1a44eadf9f555fbb13d08164e2b5
Miro <hi@miro.im>
2024-05-23 17:23:04 +0800

    lab9: update BSTmap

--- lab9/lab9/BSTMap.java
+++ lab9/lab9/BSTMap.java
@@ -1,12 +1,13 @@
 package lab9;
 
-import java.util.Iterator;
-import java.util.Set;
+import edu.princeton.cs.algs4.Queue;
+
+import java.util.*;
 
 /**
  * Implementation of interface Map61B with BST as core data structure.
  *
- * @author Your name here
+ * @author miro
  */
 public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
 
@@ -44,7 +45,20 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
      *  or null if this map contains no mapping for the key.
      */
     private V getHelper(K key, Node p) {
-        throw new UnsupportedOperationException();
+        if (p == null) {
+            return null;
+//            throw new IllegalArgumentException("calls get() with a null key");
+        }
+        if (key == null) {
+            return null;
+        }
+        if (key.compareTo(p.key) < 0) {
+            return getHelper(key, p.left);
+        } else if (key.compareTo(p.key) > 0) {
+            return getHelper(key, p.right);
+        } else {
+            return p.value;
+        }
     }
 
     /** Returns the value to which the specified key is mapped, or null if this
@@ -52,14 +66,24 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
      */
     @Override
     public V get(K key) {
-        throw new UnsupportedOperationException();
+        return getHelper(key, root);
     }
 
     /** Returns a BSTMap rooted in p with (KEY, VALUE) added as a key-value mapping.
       * Or if p is null, it returns a one node BSTMap containing (KEY, VALUE).
      */
     private Node putHelper(K key, V value, Node p) {
-        throw new UnsupportedOperationException();
+        if (p == null) {
+            return new Node(key, value);
+        }
+        if (key.compareTo(p.key) < 0) {
+            p.left = putHelper(key, value, p.left);
+        } else if (key.compareTo(p.key) > 0) {
+            p.right = putHelper(key, value, p.right);
+        } else {
+            p.value = value;
+        }
+        return p;
     }
 
     /** Inserts the key KEY
@@ -67,13 +91,17 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
      */
     @Override
     public void put(K key, V value) {
-        throw new UnsupportedOperationException();
+        if (key == null) {
+            throw new IllegalArgumentException("calls put() with a null key");
+        }
+        root = putHelper(key, value, root);
+        size += 1;
     }
 
     /* Returns the number of key-value mappings in this map. */
     @Override
     public int size() {
-        throw new UnsupportedOperationException();
+        return size;
     }
 
     //////////////// EVERYTHING BELOW THIS LINE IS OPTIONAL ////////////////
@@ -81,7 +109,11 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
     /* Returns a Set view of the keys contained in this map. */
     @Override
     public Set<K> keySet() {
-        throw new UnsupportedOperationException();
+        Set<K> set = new TreeSet<>();
+        for (K k : this) {
+            set.add(k);
+        }
+        return set;
     }
 
     /** Removes KEY from the tree if present
@@ -90,8 +122,42 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
      */
     @Override
     public V remove(K key) {
-        throw new UnsupportedOperationException();
+        return remove(root, key).value;
     }
+
+    private Node remove(Node p, K key) {
+        if (p == null || key == null) {
+            return null;
+//            throw new IllegalArgumentException("calls remove() with a null key");
+        }
+        if (key.compareTo(p.key) < 0) {
+            p.left = remove(p.left, key);
+        } else if (key.compareTo(p.key) > 0) {
+            p.right = remove(p.right, key);
+        } else {
+            if (p.right == null) {
+                return p.left;
+            }
+            if (p.left == null) {
+                return p.right;
+            }
+            Node temp = p;
+            p = min(p.right);
+            p.right = removeMin(p.right);
+            p.left = temp.left;
+        }
+        size -= 1;
+        return p;
+    }
+
+    private Node removeMin(Node p) {
+        if (p.left == null) {
+            return p.right;
+        }
+        p.left = removeMin(p.left);
+        return p;
+    }
+
 
     /** Removes the key-value entry for the specified key only if it is
      *  currently mapped to the specified value.  Returns the VALUE removed,
@@ -102,8 +168,74 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
         throw new UnsupportedOperationException();
     }
 
+    // return true if this map is empty
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
     @Override
     public Iterator<K> iterator() {
-        throw new UnsupportedOperationException();
+        if (isEmpty()) {
+            return new Queue<K>().iterator();
+        }
+        return keys(min(), max());
     }
+
+    public Iterator<K> keys(K lo, K hi) {
+        if (lo == null) {
+            throw new IllegalArgumentException("first argument to iterator() is null");
+        }
+        if (hi == null) {
+            throw new IllegalArgumentException("second argument to iterator() is null");
+        }
+        Queue<K> queue = new Queue<>();
+        keys(root, queue, lo, hi);
+        return queue.iterator();
+    }
+
+    private void keys(Node p, Queue<K> queue, K lo, K hi) {
+        if (p == null) {
+            return;
+        }
+        if (lo.compareTo(p.key) < 0) {
+            keys(p.left, queue, lo, hi);
+        }
+        if (lo.compareTo(p.key) <= 0 && hi.compareTo(p.key) >= 0){
+            queue.enqueue(p.key);
+        }
+        if (hi.compareTo(p.key) > 0) {
+            keys(p.right, queue, lo, hi);
+        }
+    }
+
+    // return the greatest key
+    private Node max(Node p) {
+        if (p.right == null) {
+            return p;
+        }
+        return max(p.right);
+    }
+
+    public K max() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("calls max() with empty map");
+        }
+        return max(root).key;
+    }
+
+    // return the smallest key
+    private Node min(Node p) {
+        if (p.left == null) {
+            return p;
+        }
+        return min(p.left);
+    }
+
+    public K min() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("calls min() with empty map");
+        }
+        return min(root).key;
+    }
+
 }
